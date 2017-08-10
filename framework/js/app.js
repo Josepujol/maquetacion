2d271318c5a1b43c72a3ca66b3ee29f52c634b71
Moises Yance <myance@jne.gob.pe>
2017-08-09 21:06:04 -0500

    cmabios de js

--- framework/js/app.js
+++ framework/js/app.js
@@ -6,6 +6,9 @@ var fullstack = (function($, undefined) {
     dom = {};
     beforeCatchDom = function() {
         fn.verificarEcma()
+        fn.eliminarParametros()
+        var migato = new fn.miGato("Michifu", "azul", 2)
+        // alert(migato.color)
     };
     catchDom = function() {
     };
@@ -16,7 +19,41 @@ var fullstack = (function($, undefined) {
     events = {
     };
     fn = {
-        
+        miGato: function (nombre, color, edad) {
+            this.nombre = new String(nombre);
+            this.color = new String(color);
+            this.edad = new Number(edad);
+            if (isNaN(this.edad)) {
+                alert("Error en el data-typing, edad no es un numero");
+            }            
+        },
+        eliminarParametros : function(){
+            var d1 = [{"id1": 1,"id3":3},{"id2":2,"id4":4}, {"id3":3,"id5":5}]
+            var filter = ["id1","id2"]            
+            for(var m in d1){
+                // console.log(d1[m])
+                for(var n in d1[m]){
+                    // console.log(n)
+                    for (var l in filter){
+                        if( n == filter[l]){
+                            delete d1[m][n]
+                        }
+                    }
+                }
+            }
+
+             console.log(d1)
+
+
+           // for(var i= 0 ; i < d1.length; i++){
+           //      // console.log(d1[i])
+           //      for(var m in filter){
+           //          console.log(filter[m])
+           //      }
+
+           //  }
+
+        },
         verificarEcma: function(){
             var ES1 = !!(Array.prototype && Array.prototype.join),
             ES3     = !!(Array.prototype && Array.prototype.pop),
